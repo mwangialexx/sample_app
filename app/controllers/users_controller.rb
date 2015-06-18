1a48a2e8bd737f987452ecd10ab297cd2597bb6d
mwangialexx <mwangialex@outlook.com>
2015-06-18 13:35:44 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts	= @user.microposts.paginate(page: params[:page])
   end
+
   def new
     @user = User.new
   end
-   def create
+
+  def create
     @user = User.new(user_params)
     if @user.save
     @user.send_activation_email
@@ -53,14 +56,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end
-  # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
   def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -61,6 +62,12 @@ validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   #Returns true if password reset has expired
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  #Defines a proto-feed
+  #See "Following users" for the full implementaion
+  def feed
+    Micropost.where("user_id = ?", id)
   end
 
   private
